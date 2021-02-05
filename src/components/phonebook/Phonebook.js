2d50b5aff14651862a9f0cfd9f2cd6014c15ed19
Dmytro Chernovskyi <dimcher911@gmail.com>
2021-02-05 11:32:42 +0200

    Корректирует ре-рендер при вводе существующего контакта

--- src/components/phonebook/Phonebook.js
+++ src/components/phonebook/Phonebook.js
@@ -1,18 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {loadContactsOperation} from "../../redux/operations/contactOperations";
 import contactsSelector from "../../redux/selectors/contacts-selectors";
 import ContactForm from "./contactForm/ContactForm";
 import Filter from "./filter/Filter";
 import ContactList from "./contactList/ContactList";
-import Notification from "./notification/Notification";
 import {Div, H1} from "./PhonebookStyled";
 
 const Phonebook = () => {
-    const [state, setState] = useState({
-        showNotification: false,
-    });
-
     const contacts = useSelector(contactsSelector.getContacts);
     const loading = useSelector(contactsSelector.getLoadingStatus);
     const dispatch = useDispatch();
@@ -21,19 +16,12 @@ const Phonebook = () => {
         dispatch(loadContactsOperation());
     }, []);
 
-    const contactExists = () => {
-        setState({showNotification: true});
-        setTimeout(() => {
-            setState({showNotification: false});
-        }, 4000);
-    };
-
     return (
         <Div>
             <H1 in={true} appear timeout={500}>
                 {loading ? "Loading..." : "Phonebook"}
             </H1>
-            <ContactForm contacts={contacts} contactExists={contactExists} />
+            <ContactForm contacts={contacts} />
             {contacts.length > 0 && (
                 <>
                     <h2>Contacts</h2>
@@ -41,7 +29,6 @@ const Phonebook = () => {
                 </>
             )}
             <ContactList />
-            <Notification showNotification={state.showNotification} />
         </Div>
     );
 };
